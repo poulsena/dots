c9e59662cf53e94fb5120e88503277125c07fb9c
Andreas Poulsen <andreas@poulsena.dk>
2023-10-21 13:31:16 +0200

    Neotree show hidden files by default

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -2,6 +2,16 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
 
   opts = function(_, opts)
+    opts.filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+      never_show = {},
+    }
+
     opts.event_handlers = {
       {
         event = "file_opened",
