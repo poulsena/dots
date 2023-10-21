c7d9612687e2093068c14643f302b25cf81469eb
Andreas Poulsen <andreas@poulsena.dk>
2023-10-21 10:58:00 +0200

    Close neotree when opening file

---
+++ .config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+
+  opts = function(_, opts)
+    opts.event_handlers = {
+      {
+        event = "file_opened",
+        handler = function(file_path)
+          -- auto close
+          -- vimc.cmd("Neotree close")
+          -- OR
+          require("neo-tree.command").execute({ action = "close" })
+        end,
+      },
+    }
+  end,
+}
